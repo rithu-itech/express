4ec06ab3e526207efd6454569d469c7edb56e1b3
rithu-itech <srithu98@gmail.com>
2024-02-03 12:39:55 +0530

    2nd task

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const http=require('http');
 const express = require('express');
+const bodyparser=require('body-parser')
 const app=express();
-app.use((req,res,next)=> {
-    console.log('In middleware');
-    next();
-}); 
-app.use((req,res,next)=> {
-    console.log('In another middleware');
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=> {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>')
+});
+app.post('/product',(req,res,next) =>{
+    console.log(req.body)
+    res.redirect('/')
+}) 
+app.use('/',(req,res,next)=> {
     res.send('<h1>Hello from node js</h1>')
 }); 
-// const server = http.createServer(app);
-// server.listen(3000)
+
 app.listen(3000)
