d97878eb029f8cf0b7ac9c2aa138e6db55b277b8
rithu-itech <srithu98@gmail.com>
2024-02-05 12:45:34 +0530

    task 3

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const express = require('express');
 const bodyparser=require('body-parser')
 const app=express();
 
+const adminroutes=require('./routes/admin');
+const shoproutes=require('./routes/shop');
 app.use(bodyparser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=> {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>')
-});
-app.post('/product',(req,res,next) =>{
-    console.log(req.body)
-    res.redirect('/')
-}) 
-app.use('/',(req,res,next)=> {
-    res.send('<h1>Hello from node js</h1>')
-}); 
+app.use('/admin',adminroutes)
+app.use(shoproutes)
 
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>')
+})
 app.listen(3000)
