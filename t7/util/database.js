71065664237c9dd8dab1914bcde5bebd8921577e
rithu-itech <srithu98@gmail.com>
2024-03-14 11:52:17 +0530

    sqliz start

--- t7/util/database.js
+++ t7/util/database.js
@@ -1,8 +1,8 @@
-const mysql=require('mysql2');
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'1234'
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
-module.exports=pool.promise();
+
+module.exports = sequelize;
